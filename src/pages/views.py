ba8bac329df366f50a97ef634215e865216c2491
eflores <dani19floress@gmail.com>
2026-05-14 20:50:15 -0600

    Add educational context to Python version in home view

--- src/pages/views.py
+++ src/pages/views.py
@@ -9,7 +9,7 @@ def home(request):
     context = {
         "debug": settings.DEBUG,
         "django_ver": get_version(),
-        "python_ver": os.environ["PYTHON_VERSION"],
+        "python_ver": os.environ["PYTHON_VERSION"] + " Fundamentos de Linux e Introducción a Django ",
     }
 
     return render(request, "pages/home.html", context)
