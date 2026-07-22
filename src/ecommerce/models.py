1ff74d301162f27f1ad36de3e8931fde1336a988
eflores <dani19floress@gmail.com>
2026-07-21 19:57:26 -0600

    Add slug field and pre-save signal for ProductModel to handle slug generation

--- src/ecommerce/models.py
+++ src/ecommerce/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.db.models.signals import pre_save
 from base.models import BasePublishedModel
 from .validators import validate_blocked_words
 
@@ -12,7 +12,24 @@ class ProductModel(BasePublishedModel):
     seller = models.CharField(max_length=100, blank=True, null=True)
     color = models.CharField(max_length=50, blank=True, null=True)
     product_dimensions = models.CharField(max_length=100, blank=True, null=True)
+    slug = models.SlugField(db_index=True, blank=True, null=True)
+
+    def get_absolute_url(self):
+        return f"/products/{self.slug}/"
     
     def save(self, *args, **kwargs):
         validate_blocked_words(self.title)
         super().save(*args, **kwargs)
+
+
+def slug_pre_save(sender, instance, *args, **kwargs):
+    if instance.slug is None or instance.slug == "":
+        new_slug = slugify(instance.title)
+        MyClass = instance.__class__
+        qs = MyClass.objects.filter(slug=new_slug).exclude(id=instance.id)
+        if qs.exists() == 0:
+            instance.slug = new_slug
+        else:
+            instance.slug = f"{new_slug}-{qs.count() + 1}"
+
+pre_save.connect(slug_pre_save, sender=ProductModel)
