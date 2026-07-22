44b78835c0bf202393274e259c6f2eff82ca96e5
eflores <dani19floress@gmail.com>
2026-07-21 20:01:04 -0600

    Refactor slug generation logic in ProductModel to improve clarity and fix slug count increment

--- src/ecommerce/models.py
+++ src/ecommerce/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.db.models.signals import pre_save
+from django.utils.text import slugify
 from base.models import BasePublishedModel
 from .validators import validate_blocked_words
 
@@ -25,11 +26,11 @@ class ProductModel(BasePublishedModel):
 def slug_pre_save(sender, instance, *args, **kwargs):
     if instance.slug is None or instance.slug == "":
         new_slug = slugify(instance.title)
-        MyClass = instance.__class__
-        qs = MyClass.objects.filter(slug=new_slug).exclude(id=instance.id)
+        MyModel = instance.__class__
+        qs =    MyModel.objects.filter(slug=new_slug).exclude(id=instance.id)
         if qs.exists() == 0:
             instance.slug = new_slug
         else:
-            instance.slug = f"{new_slug}-{qs.count() + 1}"
+            instance.slug = f"{new_slug}-{qs.count()}"
 
 pre_save.connect(slug_pre_save, sender=ProductModel)
